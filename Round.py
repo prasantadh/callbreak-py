f04857f8ddf8b6c3e6132e83b2bcd5d1d5d0d3b4
Kalyan Parajuli <kalyanrparajuli@gmail.com>
2020-04-06 00:58:14 -0400

    Rename Round class

--- Round.py
+++ Round.py
@@ -1,8 +1,11 @@
-class CallBreakRound:
+from Haat import Haat
+
+class Round:
 
     def __init__(self, orderdPlayers, deck):
         self.players=orderedPlayers #first player starts the round
         self.shuffledDeck=deck
+        self.totalPlays=13
         self._playersBid=[0,0,0,0]
         self._playersHandsWon=[0,0,0,0]
         self._playerScore=[0,0,0,0]
