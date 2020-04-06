53fbfe896caade6ba4442f46be7c20bfba78d1e1
Aashish Tripathee <aashish.tripathee@ligo.org>
2020-04-05 22:07:17 -0400

    Change _ to camelCase

--- Card.py
+++ Card.py
@@ -4,22 +4,21 @@ class Card:
         self.rank = rank
         self.faceRanks = {'A': 14, 'K': 13, 'Q': 12, 'J': 11}
 
-
     def __str__(self):
         return self.suit + self.rank
 
-    def get_rank_value(self):
+    def getRankValue(self):
     	if self.rank in self.faceRanks.keys():
     		return self.faceRanks[self.rank]
     	else:
     		return int(self.rank)
 
     def __lt__(self, otherCard):
-    	return self.get_rank_value() < otherCard.get_rank_value()
+    	return self.getRankValue() < otherCard.getRankValue()
 
     def __eq__(self, otherCard):
     	if isinstance(otherCard, Card):
-    		return self.get_rank_value() == otherCard.get_rank_value()
+    		return self.getRankValue() == otherCard.getRankValue()
     	return False
 
 if __name__=='__main__':
