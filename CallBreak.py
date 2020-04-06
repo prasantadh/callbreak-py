8f97fe1bed2d9a1d7ece8196a1f52ae9d6a01157
Kalyan Parajuli <kalyanrparajuli@gmail.com>
2020-04-05 21:40:13 -0400

    Set Round info in Callbreak

--- CallBreak.py
+++ CallBreak.py
@@ -3,6 +3,8 @@ from CardGame import CardGame
 class CallBreak(CardGame):
     def __init__(self):
         super().init()
+        totalRound=5
+        currentRound=0
 
 if __name__=='__main__':
     game = CardGame('CallBreak')
