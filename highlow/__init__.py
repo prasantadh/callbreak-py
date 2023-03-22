56c0ac93328a9d3df5e8446714bcfddc9b816b82
prasant adhikari <pa1038@nyu.edu>
2023-03-22 13:47:56 -0400

    make game a global

--- highlow/__init__.py
+++ highlow/__init__.py
@@ -5,6 +5,8 @@ import os
 from flask import Flask, request
 import jsonschema
 
+game = HighLow()
+
 def create_app(test_config=None):
 
     app = Flask(__name__, instance_relative_config=True)
@@ -23,15 +25,11 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    game = None
-
     @app.route('/new', methods=['GET'])
     def new():
         global game
         try:
-            game = HighLow()
             card = game.new()
-            print("game initialization")
         except Exception as err:
             app.logger.error("Error creating a new game.")
             app.logger.error(err)
@@ -41,7 +39,7 @@ def create_app(test_config=None):
                     "reason" : "server side failure. contact the developers."
                 }
             }
-        print(game.peek_next_card())
+        app.logger.info(game.peek_next_card())
         return {
             "result" : "success",
             "data" : {
@@ -75,12 +73,31 @@ def create_app(test_config=None):
 
         result = game.call(guess)
         card = game.deal()
-        print(game.peek_next_card())
+        app.logger.info(game.peek_next_card())
         return {
             'result' : 'success',
             'data' : {
                 'card' : str(card),
                 'score': game.score
+            }
+        }
+    
+    @app.route('/status', methods=['GET'])
+    def status():
+        global game
+        if not game or not game.isOn:
+            return {
+                'result' : 'failure',
+                'data' : {
+                    'reason' : "game isn't initialized. request a /new game"
+                }
+            }
+        app.logger.info(game.peek_next_card())
+        return {
+            'result' : 'success',
+            'data' : {
+                'card' : str(game.last_dealt()),
+                'score' : game.score
             }
         }
     return app
