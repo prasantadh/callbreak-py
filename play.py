92ba7d38ca35a3e5a0fba269384cbf00787de230
prasant adhikari <pa1038@nyu.edu>
2023-03-22 14:43:31 -0400

    add url validation

--- play.py
+++ play.py
@@ -1,17 +1,29 @@
 from highlow.Client import Client as HighLowClient
+import validators
 
 import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("game",
-        choices=["callbreak", "highlow"],
-        help="choose a game to play")
+parser.add_argument('game',
+        choices=['callbreak', 'highlow'],
+        default='callbreak',
+        help="choose a game to play. default is callbreak")
+parser.add_argument("--url", 
+        required=False,
+        default='http://127.0.0.1:5000/',
+        help=('server url at which the game is available. '
+                'ex. http://127.0.0.1:5000/. full url is expected')
+        )
 
 args = parser.parse_args()
 
+if not validators.url(args.url):
+    print('Invalid URL! Remember to include http:// or https://')
+    exit()
+
 if args.game == 'highlow':
-    client = HighLowClient()
+    client = HighLowClient(args.url)
     client.run()
 
 
